c963030aab41e267b5ba8d9d49cdf5a993746c27
Alex Brown <alexdbrown2526@gmail.com>
2018-10-22 15:09:36 -0500

    locations with pins are now clickable

--- src/components/ParkMap/ParkMap.js
+++ src/components/ParkMap/ParkMap.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react'
-import GoogleMapReact from 'google-map-react';
+// import GoogleMapReact from 'google-map-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
 class ParkMap extends Component {
-    static defaultProps = {
-        center: {
-          lat: 44.977753,
-          lng: -93.2650108
-        },
-        zoom: 13
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
       };
+
+      //Clicking on a marker will display a piece of information
+      //declared in the Marker title
+
+      onMarkerClick = (props, marker) => {
+          this.setState({
+              showingInfoWindow: true,
+              activeMarker: marker,
+              selectedPlace: props
+          })
+      }
+
+      //when a marker infowindow is open, clicking anywhere on
+      //the map will close the infowidow
+      onMapClick = () => {
+          if (this.state.showingInfoWindow) {
+              this.setState({
+                  showingInfoWindow: false
+              })
+          }
+      }
+   
 
     
         
@@ -43,22 +64,34 @@ class ParkMap extends Component {
 
     render(){
         return(
-            <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-              bootstrapURLKeys={{ key: "AIzaSyAFhYjFBg61_isW4gz2G1f6ngJRAnLpe_I" }}
-              defaultCenter={this.props.center}
-              defaultZoom={this.props.zoom}
-            >
-              <AnyReactComponent
-                lat={59.955413}
-                lng={30.337844}
-                text={'Kreyser Avrora'}
-              />
-            </GoogleMapReact>
-          </div>
+            <Map google={this.props.google}
+            onClick={this.onMapClick}
+            initialCenter={{
+                lat: 44.977753,
+                lng: -93.2650108
+            }} zoom={14}>
+
+            <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+            <Marker onClick={this.onMarkerClick}
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'Northeast Park'}
+            position={{lat:45.0031, lng: -93.2413}} />
+    
+            <InfoWindow 
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+            </InfoWindow>
+          </Map>
         )
     }
 }
 
 
-export default ParkMap;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyAFhYjFBg61_isW4gz2G1f6ngJRAnLpe_I")
+  })(ParkMap)
